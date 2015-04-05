38fcaefa25e549a7616810c645983b9e2d74f7a5
silencewwt <silencewwt@gmail.com>
2015-04-05 22:01:11 +0800

    add model UserAddress， DealerAddress， ProducerAddress

--- app/models.py
+++ app/models.py
@@ -21,11 +21,17 @@ class BaseUser(UserMixin):
 
 class User(BaseUser, db.Model):
     __tablename__ = 'users'
+    # id
     id = db.Column(db.Integer, primary_key=True)
+    # 用户名
     username = db.Column(db.Unicode(20), unique=True, nullable=False)
+    # 哈希后的密码
     password_hash = db.Column('password', db.String(120), nullable=False)
+    # 手机号码
     mobile = db.Column(db.CHAR(11), unique=True, nullable=False)
+    # 邮箱
     email = db.Column(db.String(64), nullable=False)
+    # 注册时间
     created = db.Column(db.Integer, nullable=False)
 
 
@@ -41,12 +47,16 @@ class Producer(BaseUser, db.Model):
     email = db.Column(db.String(64), nullable=False)
     # 注册时间
     created = db.Column(db.Integer, nullable=False)
+    # logo图片
+    logo = db.Column(db.String(255), default='', nullable=False)
     # 法人真实姓名
     legal_person_name = db.Column(db.Unicode(10), nullable=False)
     # 法人身份证号码
     legal_person_identity = db.Column(db.CHAR(18), nullable=False)
     # 品牌厂家名称
     name = db.Column(db.Unicode(30), unique=True, nullable=False)
+    # 营业执照注册号
+    license_identity = db.Column(db.String(20), nullable=False)
     # 营业执照所在地
     license_address = db.Column(db.Unicode(30), nullable=False)
     # 营业执照期限
@@ -55,14 +65,8 @@ class Producer(BaseUser, db.Model):
     license_long_time_limit = db.Column(db.Boolean, default=False, nullable=False)
     # 营业执照副本扫描件
     license_image = db.Column(db.String(255), nullable=False)
-    # 省份 id
-    province_id = db.Column(db.Integer, nullable=False)
-    # 城市 id
-    city_id = db.Column(db.Integer, nullable=False)
-    # 区域 id
-    district_id = db.Column(db.Integer, nullable=False)
-    # 详细地址
-    address = db.Column(db.Unicode(30), nullable=False)
+    # 地址 id
+    address_id = db.Column(db.Integer, nullable=False)
     # 联系手机
     contact_mobile = db.Column(db.CHAR(11), nullable=False)
     # 联系电话
@@ -71,6 +75,7 @@ class Producer(BaseUser, db.Model):
 
 class Dealer(BaseUser, db.Model):
     __tablename__ = 'dealers'
+    # id
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.Unicode(20), unique=True, nullable=False)
     password_hash = db.Column('password', db.String(120), nullable=False)
@@ -104,3 +109,33 @@ class District(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     district = db.Column(db.Unicode(10), nullable=False)
     city_id = db.Column(db.Integer, nullable=False)
+
+
+class UserAddress(db.Model):
+    __tablename__ = 'user_addresses'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    province_id = db.Column(db.Integer, nullable=False)
+    city_id = db.Column(db.Integer, nullable=False)
+    district_id = db.Column(db.Integer, nullable=False)
+    address = db.Column(db.Unicode(30), nullable=False)
+
+
+class ProducerAddress(db.Model):
+    __tablename__ = 'producer_addresses'
+    id = db.Column(db.Integer, primary_key=True)
+    producer_id = db.Column(db.Integer, nullable=False)
+    province_id = db.Column(db.Integer, nullable=False)
+    city_id = db.Column(db.Integer, nullable=False)
+    district_id = db.Column(db.Integer, nullable=False)
+    address = db.Column(db.Unicode(30), nullable=False)
+
+
+class DealerAddress(db.Model):
+    __tablename__ = 'dealer_addresses'
+    id = db.Column(db.Integer, primary_key=True)
+    dealer_id = db.Column(db.Integer, nullable=False)
+    province_id = db.Column(db.Integer, nullable=False)
+    city_id = db.Column(db.Integer, nullable=False)
+    district_id = db.Column(db.Integer, nullable=False)
+    address = db.Column(db.Unicode(30), nullable=False)
