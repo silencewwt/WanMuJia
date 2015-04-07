fae2717bc8520eff7b9f87a6cc2c8caed0b57848
silencewwt <silencewwt@gmail.com>
2015-04-07 10:16:25 +0800

    model Dealer

--- app/models.py
+++ app/models.py
@@ -21,7 +21,7 @@ class BaseUser(UserMixin):
 
 class User(BaseUser, db.Model):
     __tablename__ = 'users'
-    # id
+    # 用户id
     id = db.Column(db.Integer, primary_key=True)
     # 用户名
     username = db.Column(db.Unicode(20), unique=True, nullable=False)
@@ -37,7 +37,7 @@ class User(BaseUser, db.Model):
 
 class Producer(BaseUser, db.Model):
     __tablename__ = 'producers'
-    # id
+    # 厂家id
     id = db.Column(db.Integer, primary_key=True)
     # 哈希后的密码
     password_hash = db.Column('password', db.String(120), nullable=False)
@@ -75,13 +75,38 @@ class Producer(BaseUser, db.Model):
 
 class Dealer(BaseUser, db.Model):
     __tablename__ = 'dealers'
-    # id
+    # 商家id
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Unicode(20), unique=True, nullable=False)
+    # 哈希后的密码
     password_hash = db.Column('password', db.String(120), nullable=False)
+    # 用于登录的手机号码
     mobile = db.Column(db.CHAR(11), unique=True, nullable=False)
+    # 邮箱
     email = db.Column(db.String(64), nullable=False)
+    # 注册时间
     created = db.Column(db.Integer, nullable=False)
+    # 法人真实姓名
+    legal_person_name = db.Column(db.Unicode(10), nullable=False)
+    # 法人身份证号码
+    legal_person_identity = db.Column(db.CHAR(18), nullable=False)
+    # 品牌厂家名称
+    name = db.Column(db.Unicode(30), unique=True, nullable=False)
+    # 营业执照注册号
+    license_identity = db.Column(db.String(20), nullable=False)
+    # 营业执照所在地
+    license_address = db.Column(db.Unicode(30), nullable=False)
+    # 营业执照期限
+    license_limit = db.Column(db.Integer, nullable=False)
+    # 长期印业执照
+    license_long_time_limit = db.Column(db.Boolean, default=False, nullable=False)
+    # 营业执照副本扫描件
+    license_image = db.Column(db.String(255), nullable=False)
+    # 地址 id
+    address_id = db.Column(db.Integer, nullable=False)
+    # 联系手机
+    contact_mobile = db.Column(db.CHAR(11), nullable=False)
+    # 联系电话
+    contact_telephone = db.Column(db.CHAR(15), nullable=False)
 
 
 class Privilege(BaseUser, db.Model):
