28a1dec710e160d20c3fe796553ce888ac7d05c7
silencewwt <silencewwt@gmail.com>
2015-04-17 09:16:18 +0800

    reconstruction redis module

--- app/utils/redis.py
+++ app/utils/redis.py
@@ -6,28 +6,24 @@ from flask import current_app
 from app import local_redis
 
 
-def redis_set(content_type, key, value=None, expire=None, **kwargs):
+def redis_set(content_type, key, value, expire=None, **kwargs):
     if content_type == 'REG_TOKEN':
-        key = '%s:%s' % (content_type, key)
         value = json.dumps(dict(
             password=kwargs['password'],
             email=kwargs['email']
         ))
-        expire = expire if expire else current_app.config['CONFIRM_EMAIL_DURATION']
-        local_redis.set(key, value, expire)
-    elif content_type == 'SMS_CAPTCHA':
-        key = '%s:%s' % (content_type, key)
-        expire = expire if expire else current_app.config['SMS_CAPTCHA_DURATION']
-        local_redis.set(key, value, expire)
+    key = '%s:%s' % (content_type, key)
+    expire = expire if expire else current_app.config['%s_DURATION' % content_type]
+    local_redis.set(key, value, expire)
 
 
 def redis_get(content_type, key, **kwargs):
-    if content_type == 'REG_TOKEN':
-        key = '%s:%s' % (content_type, key)
-        value = local_redis.get(key)
-        if value is not None:
-            value = json.loads(value)
-        return value
-    elif content_type == 'SMS_CAPTCHA':
-        key = '%s:%s' % (content_type, key)
-        return local_redis.get(key)
+    key = '%s:%s' % (content_type, key)
+    value = local_redis.get(key)
+    if content_type == 'REG_TOKEN' and value is not None:
+        value = json.loads(value)
+    return value
+
+
+def redis_verify(content_type, key, value):
+    return value == redis_get(content_type, key)
