3a4ab8d1bdc79a83d6fbdd69aeb75d26b79e51cb
silencewwt <silencewwt@gmail.com>
2015-04-21 15:45:47 +0800

    add sms_captcha_generator

--- app/utils/captcha.py
+++ app/utils/captcha.py
@@ -4,19 +4,26 @@ import string
 
 from captcha.image import ImageCaptcha
 
+from redis import redis_set
 from ..tasks import send_sms
 
 ic = ImageCaptcha()
 
 
-def id_generator(size=4, chars=string.ascii_uppercase + string.digits[1:]):
+def id_generator(size=4, chars=None):
+    chars = chars or string.ascii_uppercase + string.digits[1:]
     return ''.join(random.SystemRandom().choice(chars) for _ in range(size))
 
 
-def image_captcha_generator(chars=id_generator()):
-    captcha_output = ic.generate_image(chars)
+def image_captcha_generator():
+    captcha_output = ic.generate_image(id_generator())
     return captcha_output
 
 
+def sms_captcha_generator():
+    return id_generator(6, string.digits)
+
+
 def send_captcha(type_, mobile):
+    captcha_chars = sms_captcha_generator()
     pass
