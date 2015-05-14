2273ce9dc68ce2a4023678280f23c6b3294252d5
silencewwt <silencewwt@gmail.com>
2015-05-14 22:23:17 +0800

    add producer register

--- app/producer/forms.py
+++ app/producer/forms.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
+from cStringIO import StringIO
+from PIL import Image
 from flask.ext.wtf import Form
 from flask.ext.wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import ValidationError, DataRequired, Length, EqualTo
 
+from app import db
+from app.models import Producer, District, ProducerAddress
+from app.utils import save_image
 from app.utils.validator import Email, Mobile
 
 
@@ -12,18 +17,16 @@ class LoginForm(Form):
     password = PasswordField(validators=[DataRequired()])
 
 
-class RegistrationForm(Form):
+class RegistrationDetailForm(Form):
     email = StringField(validators=[Email()])
-    mobile = StringField(validators=[Mobile()])
     password = PasswordField(validators=[DataRequired(), Length(6, 32), EqualTo('confirm_password')])
     confirm_password = PasswordField(validators=[DataRequired(), Length(6, 32)])
     legal_person_name = StringField(validators=[DataRequired(u'必填')])
     legal_person_identity = StringField(validators=[DataRequired(u'必填'), Length(18, 18, u'身份证号码不符合规范!')])
-    legal_person_identity_image = FileField(validators=[
+    legal_person_identity_front = FileField(validators=[
         FileRequired(u'必填'), FileAllowed(['jpg', 'png'], u'只支持jpg, png!')])
-
-
-class ProducerDetailForm(Form):
+    legal_person_identity_back = FileField(validators=[
+        FileRequired(u'必填'), FileAllowed(['jpg', 'png'], u'只支持jpg, png!')])
     name = StringField(validators=[DataRequired(u'必填'), Length(2, 30, u'品牌厂商名称不符合规范')])
     license_address = StringField(validators=[DataRequired(u'必填')])
     license_limit = StringField(validators=[Length(8, 8)])
@@ -32,8 +35,59 @@ class ProducerDetailForm(Form):
     contact_mobile = StringField(validators=[DataRequired(u'必填'), Mobile(available=False)])
     contact_telephone = StringField(validators=[DataRequired(u'必填'), Length(7, 15)])
     address = StringField(validators=[DataRequired(u'必填'), Length(1, 30)])
+    address_id = StringField(validators=[DataRequired(), Length(6, 6)])
+
     # TODO: add address select
 
     def validate_license_limit(self, field):
         if not field.data and not self.license_long_time_limit.data:
             raise ValidationError(u'请填写营业执照期限或选择长期营业执照')
+
+    def validate_legal_person_identity_front(self, field):
+        try:
+            im = Image.open(StringIO(field.data))
+            im.verify()
+        except:
+            raise ValidationError(u'图片格式错误')
+
+    def validate_legal_person_identity_back(self, field):
+        try:
+            im = Image.open(StringIO(field.data))
+            im.verify()
+        except:
+            raise ValidationError(u'图片格式错误')
+
+    def validate_license_image(self, field):
+        try:
+            im = Image.open(StringIO(field.data))
+            im.verify()
+        except:
+            raise ValidationError(u'图片格式错误')
+
+    def add_producer(self, mobile):
+        district = District.query.filter_by(cn_id=self.address_id)
+        address = ProducerAddress(producer_id='', district_id=district.id, address=self.address.data)
+        db.session.add(address)
+        db.session.commit()
+        producer = Producer(
+            password=self.password.data,
+            email=self.password.data,
+            mobile=mobile,
+            legal_person_name=self.legal_person_name.data,
+            legal_person_identity=self.legal_person_identity.data,
+            license_address=self.license_address.data,
+            license_limit=self.license_limit.data,
+            license_long_time_limit=self.license_long_time_limit.data,
+            name=self.name.data,
+            contact_mobile=self.contact_mobile.data,
+            contact_telephone=self.contact_telephone.data,
+            address_id=address.id
+        )
+        db.session.add(producer)
+        db.session.commit()
+        identity_front = save_image(producer.id, self.legal_person_identity_front)
+        identity_back = save_image(producer.id, self.legal_person_identity_back)
+        license_image = save_image(producer.id, self.license_image)
+        producer.legal_person_identity_front = identity_front
+        producer.legal_person_identity_back = identity_back
+        producer.license_image = license_image
--- app/producer/views.py
+++ app/producer/views.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
-from flask import current_app, flash, render_template, redirect
+from flask import current_app, flash, render_template, redirect, request, session
 from flask_security import login_user, logout_user, login_required
 from flask_security.utils import identity_changed, Identity
 
 from app import db
-from app.core import login as model_login
+from app.core import login as model_login, reset_password as model_reset_password
 from app.models import Producer
 from .import producer as producer_blueprint
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationDetailForm
+from app.forms import MobileRegistrationForm
+from app.constants import *
 
 
 @producer_blueprint.route('/login', methods=['GET', 'POST'])
@@ -18,3 +20,28 @@ def login():
             return redirect('/')    # TODO: redirect
         flash(u'用户名或密码错误!')
     return render_template('user/login.html', login_form=form)
+
+
+@producer_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    form_type = request.args.get('form', '', type=str)
+    mobile_form = MobileRegistrationForm()
+    detail_form = RegistrationDetailForm()
+    if PRODUCER_REGISTER_STEP_DONE in session:
+        if PRODUCER_REGISTER_STEP_DONE == 0:
+            if form_type == 'mobile' and mobile_form.validate_on_submit():
+                session[PRODUCER_REGISTER_MOBILE] = mobile_form.mobile.data
+                session[PRODUCER_REGISTER_STEP_DONE] = 1
+                return 'step 2 page'
+            return 'step 1 page'
+        elif PRODUCER_REGISTER_STEP_DONE == 1:
+            if form_type == 'detail' and detail_form.validate_on_submit():
+                producer = detail_form.add_producer(session[PRODUCER_REGISTER_MOBILE])
+                login_user(producer)
+                identity_changed.send(current_app._get_current_object(), Identity(producer.get_id()))
+                session.pop(PRODUCER_REGISTER_MOBILE)
+                session.pop(PRODUCER_REGISTER_STEP_DONE)
+                return 'register done'
+            return 'step 2 page'
+    session[PRODUCER_REGISTER_STEP_DONE] = 0
+    return 'step 1 page'
