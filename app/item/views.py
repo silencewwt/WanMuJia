0a6bd6dfa55b98ee3e8b1fe76cd9c0dcbae1496d
silencewwt <silencewwt@gmail.com>
2015-05-18 09:06:28 +0800

    add item compare

--- app/item/views.py
+++ app/item/views.py
@@ -23,4 +23,10 @@ def detail():
 
 @item_blueprint.route("/compare")
 def compare():
-    pass
+    first_id = request.args.get('first', 0, type=int)
+    second_id = request.args.get('second', 0, type=int)
+    if first_id == second_id:
+        pass  # TODO: 提示？
+    first = Item.query.get_or_404(first_id)
+    second = Item.query.get_or_404(second_id)
+    return render_template("/item/compare.html", first=first, second=second)
