12154eee42b56b5fad69132d9ff10a16faf382a8
silencewwt <silencewwt@gmail.com>
2015-06-03 20:18:01 +0800

    add md5_with_timestamp_salt, _data_convert

---
+++ app/utils/_utils.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import hashlib
+import random
+import time
+
+from flask import current_app
+
+from ._compat import PY3
+
+random.seed()
+
+
+def md5(data):
+    return hashlib.md5(_data_convert(data)).hexdigest()
+
+
+def md5_with_salt(data):
+    return md5(_data_convert(data, current_app.config['MD5_SALT']))
+
+
+def md5_with_timestamp_salt(data):
+    return md5_with_salt(_data_convert(data, time.time()))
+
+
+def _data_convert(*args):
+    data = ''.join(list(map(str, args)))
+    if PY3:
+        data = bytes(data)
+    return data
