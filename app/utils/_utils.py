b1f9937ee1d1dbf317fa8b55756fdfbec6bac989
silencewwt <silencewwt@gmail.com>
2015-06-08 14:49:08 +0800

    fix bugs

--- app/utils/_utils.py
+++ app/utils/_utils.py
@@ -10,19 +10,19 @@ from ._compat import PY3
 random.seed()
 
 
-def md5(data):
+def md5(*data):
     return hashlib.md5(_data_convert(data)).hexdigest()
 
 
-def md5_with_salt(data):
-    return md5(_data_convert(data, current_app.config['MD5_SALT']))
+def md5_with_salt(*data):
+    return md5(_data_convert(data + (current_app.config['MD5_SALT'],)))
 
 
-def md5_with_timestamp_salt(data):
-    return md5_with_salt(_data_convert(data, time.time()))
+def md5_with_timestamp_salt(*data):
+    return md5_with_salt(_data_convert(data + (time.time(),)))
 
 
-def _data_convert(*args):
+def _data_convert(args):
     data = ''.join(list(map(str, args)))
     if PY3:
         data = bytes(data)
