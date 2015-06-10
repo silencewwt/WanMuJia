23eaaed40e839759f44547198e28ad2d9fe62dbf
silencewwt <silencewwt@gmail.com>
2015-06-10 21:22:37 +0800

    update md5

--- app/utils/_utils.py
+++ app/utils/_utils.py
@@ -10,20 +10,20 @@ from ._compat import PY3
 random.seed()
 
 
-def md5(*data):
-    return hashlib.md5(_data_convert(data)).hexdigest()
+def md5(data):
+    return hashlib.md5(data).hexdigest()
 
 
 def md5_with_salt(*data):
     return md5(_data_convert(data + (current_app.config['MD5_SALT'],)))
 
 
-def md5_with_timestamp_salt(*data):
-    return md5_with_salt(_data_convert(data + (time.time(),)))
+def md5_with_time_salt(*data):
+    return md5(_data_convert(data + (current_app.config['MD5_SALT'], time.time())))
 
 
 def _data_convert(args):
     data = ''.join(list(map(str, args)))
     if PY3:
-        data = bytes(data)
+        return bytes(data, 'utf8')
     return data
