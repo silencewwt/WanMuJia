3767a114f398f1a9c8c97d37213d7be96015248b
silencewwt <silencewwt@gmail.com>
2015-07-25 23:45:45 +0800

    add EmailForm

--- app/service/forms.py
+++ app/service/forms.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
-from wtforms import StringField
+from wtforms import StringField, IntegerField
 
-from app.constants import SMS_CAPTCHA
+from app.constants import SMS_CAPTCHA, CONFIRM_EMAIL
+from app.models import User, Vendor
 from app.forms import Form
+from app.wmj_email import send_email, USER_EMAIL_CONFIRM, VENDOR_EMAIL_CONFIRM
+from app.utils import md5_with_time_salt
 from app.utils.myj_captcha import send_sms_captcha
-from app.utils.validator import Mobile, Captcha
+from app.utils.redis import redis_set
+from app.utils.validator import Mobile, Captcha, ValidationError
 
 
 class MobileRegisterSMSForm(Form):
@@ -17,3 +21,30 @@ class MobileRegisterSMSForm(Form):
 
     def send_sms(self):
         send_sms_captcha(self.template, self.mobile.data)
+
+
+class EmailForm(Form):
+    role = StringField()
+    id = IntegerField()
+
+    email = None
+    email_confirmed = False
+
+    def validate_id(self, field):
+        role = None
+        if self.role == 'user':
+            role = User.query.get(field.data)
+        elif self.role == 'vendor':
+            role = Vendor.query.get(field.data)
+        if not role:
+            raise ValidationError(u'没有此用户')
+        self.email = role.email
+        self.email_confirmed = role.email_confirmed
+
+    def send_email(self, email_type):
+        if (email_type == VENDOR_EMAIL_CONFIRM or email_type == USER_EMAIL_CONFIRM) and not self.email_confirmed:
+            token = md5_with_time_salt(self.role.data, self.id.data)
+            redis_set(CONFIRM_EMAIL, token, 86400)
+            url = '%s/service/?token=%s' % ('http://wanmujia.com', token)
+            send_email(self.email, '', email_type, url=url)
+
