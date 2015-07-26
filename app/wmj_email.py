e31ee36db8d2b91e9a0a909cdf7e09ae30cfb8bf
silencewwt <silencewwt@gmail.com>
2015-07-26 23:17:03 +0800

    fixed bug

--- app/wmj_email.py
+++ app/wmj_email.py
@@ -2,10 +2,14 @@
 from flask.ext.mail import Message
 
 from app import app
-from tasks import send_email as async_email
+from app.tasks import send_email as async_email
 
 
-def send_email(to, subject, template, **kwargs):
+VENDOR_EMAIL_CONFIRM = 'VENDOR_EMAIL_CONFIRM'
+USER_EMAIL_CONFIRM = 'USER_EMAIL_CONFIRM'
+
+
+def send_email(to, subject, email_type, **kwargs):
     msg = Message(app.config['WMJ_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
                   sender=app.config['WMJ_MAIL_SENDER'], recipients=[to])
     msg.body = '万木家'
