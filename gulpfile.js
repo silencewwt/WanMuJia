c724d4196f699e0a5ac2b49226217260afac5283
proudrain <lyhproud@163.com>
2015-10-07 15:09:17 +0800

    [*] update gulp workflow.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var stylish = require('jshint-stylish');
+var merged = require('merge-stream');
 var $ = require('gulp-load-plugins')({
     rename: {
         'gulp-minify-css': 'minifyCss',
@@ -13,7 +14,7 @@ var conf = {
     serverPath: '../MuYiJia/app/',
     staticPath: './fe-src/static/',
     pagePath: './fe-src/pages/',
-    distPath: './app/',
+    distPath: './app/'
 };
 
 
@@ -28,6 +29,7 @@ gulp.task('sass', function () {
 
 gulp.task('js', function () {
     return gulp.src(conf.staticPath + './js/**/*.js')
+        .pipe($.react())
         .pipe($.jshint())
         .pipe($.jshint.reporter(stylish))
         .pipe(gulp.dest(conf.serverPath + './static/js'));
@@ -39,19 +41,21 @@ gulp.task('img', function () {
 });
 
 gulp.task('lib', function () {
-    gulp.src(
+    return merged(
+        gulp.src(
             ['./lib/*.js', './lib/*.css']
             .map(function (src) {
                 return conf.staticPath + src;
             })
         )
-        .pipe(gulp.dest(conf.serverPath + './static/lib'));
+        .pipe(gulp.dest(conf.serverPath + './static/lib')),
 
-    gulp.src(conf.staticPath + './lib/fonts/**')
-        .pipe(gulp.dest(conf.serverPath + './static/lib/fonts'));
+        gulp.src(conf.staticPath + './lib/fonts/**')
+            .pipe(gulp.dest(conf.serverPath + './static/lib/fonts')),
 
-    gulp.src(conf.staticPath + './lib/xenon/**')
-        .pipe(gulp.dest(conf.serverPath + './static/lib/xenon'));
+        gulp.src(conf.staticPath + './lib/xenon/**')
+            .pipe(gulp.dest(conf.serverPath + './static/lib/xenon'))
+    );
 });
 
 gulp.task('pages', function () {
@@ -60,15 +64,17 @@ gulp.task('pages', function () {
 });
 
 gulp.task('compress', ['sass', 'js', 'img', 'lib', 'pages'], function () {
-    gulp.src(conf.serverPath + './static/css/**/*.css')
-        .pipe($.minifyCss())
-        .pipe(gulp.dest(conf.distPath + './static/css'));
-    gulp.src(conf.serverPath + './static/js/**/*.js')
-        .pipe($.uglify())
-        .pipe(gulp.dest(conf.distPath + './static/js'));
-    gulp.src(conf.serverPath + './templates/**/**')
-        // .pipe($.minifyHtml())
-        .pipe(gulp.dest(conf.distPath + './templates'));
+    return merged(
+        gulp.src(conf.serverPath + './static/css/**/*.css')
+            .pipe($.minifyCss())
+            .pipe(gulp.dest(conf.distPath + './static/css')),
+        gulp.src(conf.serverPath + './static/js/**/*.js')
+            .pipe($.uglify())
+            .pipe(gulp.dest(conf.distPath + './static/js')),
+        gulp.src(conf.serverPath + './templates/**/**')
+            // .pipe($.minifyHtml())
+            .pipe(gulp.dest(conf.distPath + './templates'))
+    );
 });
 
 
@@ -77,11 +83,12 @@ gulp.task('server', ['sass', 'js', 'img', 'lib', 'pages']);
 gulp.task('release', ['compress'], function () {
     // gulp.src(conf.serverPath + './static/lib/fonts/**')
     //     .pipe(gulp.dest(conf.distPath + './static/lib/fonts'));
-
-    gulp.src(conf.serverPath + './static/img/**/**')
-        .pipe(gulp.dest(conf.distPath + './static/img'));
-    gulp.src(conf.serverPath + './static/lib/**/**')
-        .pipe(gulp.dest(conf.distPath + './static/lib'));
+    return merged(
+        gulp.src(conf.serverPath + './static/img/**/**')
+            .pipe(gulp.dest(conf.distPath + './static/img')),
+        gulp.src(conf.serverPath + './static/lib/**/**')
+            .pipe(gulp.dest(conf.distPath + './static/lib'))
+    );
 });
 
 
