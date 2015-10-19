b75882aeb0280ad67141b1d7c468a23688104d58
proudrain <lyhproud@163.com>
2015-10-19 15:39:28 +0800

    [user] release.

--- app/static/js/user/tab.js
+++ app/static/js/user/tab.js
@@ -1,1 +1,1 @@
-$(function(){$(".tab .tabs").delegate("li a","click",function(){var a=$(this).parent(),t=a.data("tab");a.siblings().removeClass("active"),a.addClass("active"),a.parent().nextAll(".tab-content").find("."+t).show().siblings().hide()})});
+$(function(){window.location.hash.slice(1);$(".tab .tabs").delegate("li a","click",function(){var a=$(this).parent(),i=a.data("tab");a.siblings().removeClass("active"),a.addClass("active"),window.location.hash=i,a.parent().nextAll(".tab-content").find("."+i).show().siblings().hide()})});
