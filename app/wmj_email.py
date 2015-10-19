3eddea5ccd732ee86722ec341307b28787008a5f
Silence <silencewwt@gmail.com>
2015-10-19 16:26:06 +0800

    update send_email

--- app/wmj_email.py
+++ app/wmj_email.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import current_app
+from flask import current_app, render_template
 from flask.ext.mail import Message
 
 from app.tasks import send_email as async_email
@@ -12,26 +12,19 @@ USER_RESET_PASSWORD = 'USER_RESET_PASSWORD'
 ADMIN_REMINDS = 'ADMIN_REMINDS'
 ADMIN_REMINDS_SUBJECT = '新的厂家注册'
 EMAIL_CONFIRM_SUBJECT = u'激活邮箱'
-EMAIL_CONFIRM_TEMPLATE = '<p>尊敬的万木家用户:</p><p>感谢注册万木家, 请点击下面的链接以激活邮箱</p><a href="%s">%s</a>'
 
 
 def send_email(to, subject, email_type, **kwargs):
-    if email_type == VENDOR_EMAIL_CONFIRM or email_type == USER_EMAIL_CONFIRM:
-        msg = Message(subject, sender=current_app.config['WMJ_MAIL_SENDER'], recipients=[to])
-        msg.body = '万木家'
-        msg.html = EMAIL_CONFIRM_TEMPLATE % (kwargs['url'], kwargs['url'])
-        async_email.delay(msg)
+    msg = Message(subject, sender=current_app.config['WMJ_MAIL_SENDER'], recipients=[to])
+    if email_type == VENDOR_EMAIL_CONFIRM:
+        msg.html = render_template('site/vendor_email_confirm.html', url=kwargs['url'])
+    elif email_type == USER_EMAIL_CONFIRM:
+        msg.html = render_template('site/user_email_confirm.html', url=kwargs['url'])
     elif email_type == ADMIN_REMINDS:
-        msg = Message(subject, sender=current_app.config['WMJ_MAIL_SENDER'], recipients=to)
         msg.html = '<p>有新的厂家注册了, 快去审核!</p>'
-        async_email.delay(msg)
     elif email_type == USER_REGISTER:
-        msg = Message(subject, sender=current_app.config['WMJ_MAIL_SENDER'], recipients=[to])
-        msg.body = '万木家'
-        msg.html = EMAIL_CONFIRM_TEMPLATE % (kwargs['url'], kwargs['url'])
+        msg.html = render_template('site/user_register.html', url=kwargs['url'])
         async_email.delay(msg)
     elif email_type == USER_RESET_PASSWORD:
-        msg = Message(subject, sender=current_app.config['WMJ_MAIL_SENDER'], recipients=[to])
-        msg.body = '万木家'
-        msg.html = EMAIL_CONFIRM_TEMPLATE % (kwargs['url'], kwargs['url'])
-        async_email.delay(msg)
+        msg.html = render_template('site/user_reset_password.html', url=kwargs['url'])
+    async_email.delay(msg)
