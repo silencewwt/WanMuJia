dae0d8cf4cc13bbb63666ae2acec77443147846c
TongchengQiu@gmail.com <qiutc@qiutcdeMacBook-Pro.local>
2015-11-16 14:38:57 +0800

    [user] init login page & loginIn(base) components

---
+++ app/views/Login/Login.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+require('../../assets/pages/login.html');
+require('./Login.scss');
+
+require('../../assets/images/favicon.png');
+
+let utils = require('../../lib/utils/utils');
+
+let React = require('react');
+let ReactDOM = require('react-dom');
