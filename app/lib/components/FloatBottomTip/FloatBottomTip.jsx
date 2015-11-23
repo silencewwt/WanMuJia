78afdfaa29839e9578a28c6fdb73ef7d53c25c0f
TongchengQiu <TongchengQiu@gmail.com>
2015-11-23 21:24:04 +0800

    [user] complete FloatBottomTip & BackTop

--- app/lib/components/FloatBottomTip/FloatBottomTip.jsx
+++ app/lib/components/FloatBottomTip/FloatBottomTip.jsx
@@ -1,14 +1,16 @@
 'use strict';
 
-require('FloatBottomTip.scss');
+require('./FloatBottomTip.scss');
 
 let React = require('react');
+let BackTop = require('./BackTop/BackTop.jsx');
 
 var FloatBottomTip = React.createClass({
   render: function() {
     return (
       <div className="float-bottom-tip">
-        FloatBottomTip
+        <div>对比</div>
+        <BackTop />
       </div>
     );
   },
