eeaa53cf273712f2c376414437f1ea8792636f74
Silence <silencewwt@gmail.com>
2015-11-24 01:13:53 +0800

    [BE-index] update index api

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,6 @@ import random
 import json
 
 from flask import render_template, abort, Response
-from sqlalchemy import func
 from flask.ext.login import current_user
 
 from app import statisitc
@@ -15,24 +14,7 @@ from .import main
 
 @main.route('/')
 def index():
-    item_ids = redis_get('INDEX_ITEMS', 'ITEMS')
-    if item_ids:
-        item_ids = json.loads(item_ids)
-    else:
-        items = statisitc.item_query.order_by(func.rand()).limit(18).all()
-        item_ids = [item.id for item in items]
-        redis_set('INDEX_ITEMS', 'ITEMS', json.dumps(item_ids), expire=86400)
-    items = Item.query.filter(Item.id.in_(item_ids)).order_by(Item.id).all()
-    while len(items) < 18:
-        items.append(items[0])
-    scenes = []
-    for first_scene in Scene.query.filter_by(level=1).order_by(Scene.id):
-        l = [(first_scene.id, first_scene.scene), []]
-        for scene in Scene.query.filter_by(father_id=first_scene.id).order_by(Scene.id):
-            l[1].append((scene.id, scene.scene))
-        scenes.append(l)
-    return render_template('user/index.html', user=current_user, scenes=scenes,
-                           group1=items[:6], group2=items[6:12], group3=items[12:18])
+    return render_template('user/index.html')
 
 
 @main.route('/navbar')
