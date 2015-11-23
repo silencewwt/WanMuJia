9f0228c4b847ad76a593924f1ae7052f0a9c7c0a
TongchengQiu <TongchengQiu@gmail.com>
2015-11-23 11:35:20 +0800

    [user] init cmpareBar

---
+++ app/lib/components/CompareBar/CompareBar.jsx
@@ -0,0 +1,17 @@
+'use strict';
+require('./CompareBar.jsx');
+
+let React = require("react");
+let reqwest = require("reqwest");
+
+var CompareBar = React.createClass({
+  render: function() {
+    return (
+      <div className="compare-bar">
+        asdasdadsadsdssad
+      </div>
+    );
+  }
+});
+
+module.exports = CompareBar;
