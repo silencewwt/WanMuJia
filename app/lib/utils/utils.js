a1533f5bd249a0e0c522ae700936f9a9910716cc
proudrain <lyhproud@163.com>
2015-11-27 14:51:35 +0800

    update utils' query part.

--- app/lib/utils/utils.js
+++ app/lib/utils/utils.js
@@ -3,9 +3,10 @@
 let type = require('./type');
 let reg = require('./reg_exps');
 let cookie = require('./cookie');
-let query = require('./query_to_json');
+let query = require('./query');
 let encryption = require('./encrypt');
+let history = require('./history');
 
-let utils = Object.assign({}, type, reg, cookie, query, encryption);
+let utils = Object.assign({}, type, reg, cookie, query, encryption, history);
 
 module.exports = utils;
